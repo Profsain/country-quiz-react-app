3ad5077f22bfc01a1af67fc666986368e39d244a
profsain <husseinimudiking@gmail.com>
2023-05-23 02:19:54 +0100

    add basic home style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Country Quiz App</h1>
+      <h1 className="Headings">Country Quiz</h1>
+      {/* quiz component */}
+        {/* question component */}
+        {/* answare component */}
+      {/* result report component */}
     </div>
   );
 }
